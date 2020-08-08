7696c4e40e34bfe61036eb384f33ec0098632d90
dongmeilim <dongmeilim@gmail.com>
2020-08-08 20:43:59 +1200

    Made catalog list collapsible

--- components/Catalog.js
+++ components/Catalog.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 
+import Collapsible from 'react-collapsible';
+
 export const Catalog = ({ searchValue }) => {
-    const [data, setData] = useState([]);
+    let [data, setData] = useState([]);
+    let [expanded, setExpanded] = useState(null);
 
     useEffect(() => {
         console.log("Search value: ", searchValue)
@@ -17,22 +20,41 @@ export const Catalog = ({ searchValue }) => {
             setData(data);
         });
     }, [searchValue]);
-    
-    console.log(data);
+
+    console.log(data)
     return (
         <div>
-            {data.map(course => (<CourseItem course={course} key={course.id}/>))}
+            {data.map(course => (<CourseItem expanded={expanded} setExpanded={setExpanded} course={course} key={course.id} />))}
         </div>
 
     )
 }
 
-function CourseItem({ course }) {
-    const title = `${course.subject} ${course.catalogNbr}: ${course.title}`;
+function CourseItem({ course, expanded, setExpanded }) {
+    const handleClick = () => {
+        if (expanded != course.id) {
+            setExpanded(course.id)
+        }
+        else {
+            setExpanded(null)
+        }
+    }
+    const title = (
+        <div>
+            <span onClick={handleClick} >{`${course.subject} ${course.catalogNbr}: ${course.title}`}
+            </span>
+            <span>{`${course.year}`}</span>
+        </div>
+    )
+    const preReq = `${course.rqrmntDescr}`.replace("Prerequisite:", "");
+
     return (
-        <a href={`/course/${course.crseId}`}>
-            <div key={course.id} className={`courseItemList ${course.mainProgram}`}>{title}</div>
-        </a>
+        <Collapsible key={course.id} className={`courseItemList ${course.mainProgram}`} trigger={title} open={expanded == course.id}>
+            {course.rqrmntDescr && <p><b>Prerequisite:</b>{preReq}</p>}
+            <b>Description:</b>
+            {course.description && <p>{course.description}</p>}
+            <a href={`/course/${course.crseId}`}><u>+ full description</u></a>
+        </Collapsible>
     )
 }
 
