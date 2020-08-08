8fc80c6f45059ac222680a6210e3da9bfb76f1e0
Tom van Dinther <tvandinther@gmail.com>
2020-08-08 17:31:56 +1200

    course api now connected to UoA

--- pages/api/course.js
+++ pages/api/course.js
@@ -2,14 +2,21 @@ import nextConnect from 'next-connect'
 import {course} from "../../stubs/courseStub"
 
 const handler = nextConnect()
-
+/*
+	Expected body parameters:
+		courseid: string
+*/
 handler.post(async (req, res) => {
 	const data = JSON.parse(req.body)
-	/*
-	course id
-	*/
-
-	let url = new URL("https://api.test.auckland.ac.nz/service/courses/v2/course/" + req.body.id)
-
-	return await fetch(url)
+	
+	let url = new URL(`https://api.test.auckland.ac.nz/service/courses/v2/course/${data.courseid}`)
+	try {
+		const response = await fetch(url);
+		const responseData = await response.json()
+		res.json(responseData)
+	} catch (error) {
+		res.json({message: "error", error: error})
+	}
 })
+
+export default handler
