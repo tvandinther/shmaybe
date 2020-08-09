326eca40a392397c26441783bb6b27af2420fa7c
Tom van Dinther <tvandinther@gmail.com>
2020-08-09 14:09:39 +1200

    added comma to number

--- components/Pagination.js
+++ components/Pagination.js
@@ -14,7 +14,7 @@ export default function Pagination(props) {
 	return (
 		<div>
 			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 border border-blue-700 rounded m-2" onClick={handlePrev}>⤌</button>
-				<span>Showing results {from + 1}-{from + results} of {total}</span>
+				<span>Showing results {from + 1}-{from + results} of {total.toLocaleString()}</span>
 			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 border border-blue-700 rounded m-2" onClick={handleNext}>⤍</button>
 		</div>
 	)
